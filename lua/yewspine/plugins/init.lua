a32ec05565abc9d125db2f686e9bc54d1bc3e2ed
yewspine <yewspine.game@gmail.com>
2025-06-15 11:10:23 +0200

    initial commit

---
+++ lua/yewspine/plugins/init.lua
@@ -0,0 +1,14 @@
+return {
+  -- Core Plugins
+  { import = "yewspine.plugins.core" },
+
+  -- Tools 
+  { import = "yewspine.plugins.tools" },
+
+  -- LSP
+  { import = "yewspine.plugins.lsp" },
+
+  -- Per Language LSP
+  -- { import = "yewspine.plugins.lang" },
+}
+
